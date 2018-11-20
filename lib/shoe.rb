0cb0d8c7b69e790cd1da300b8654b23efdd3402e
Mahmoud Agamy <agamyv@gmail.com>
2018-11-20 22:55:44 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
 class Shoe
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
@@ -13,7 +14,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-
 
 end
